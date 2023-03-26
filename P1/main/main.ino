d5806f6810d0b776e3aa4a39d523a35804a680f7
psilDave <davipsilva7@gmail.com>
2023-03-25 22:51:46 -0300

    P1 - v0.02

--- P1/main/main.ino
+++ P1/main/main.ino
@@ -9,7 +9,13 @@ volatile unsigned int botao_pedestre = 0;
 // periodo_do_dia: Variável responsável por armazenar o valor regrado pelo LDR indicando o período do dia. 
 // Valores possíveis:
 // "0" - Dia | "1"- Noite.
-volatile unsigned int periodo_do_dia  = 0; 
+volatile unsigned int periodo_do_dia  = 0;
+
+
+// PINOS IMPORTANTES
+int pino_LDR = 14;
+int pino_botao_pedestre = 6;
+
 
 // CONFIGURAÇÃO DAS INTERRUPÇÕES PERIÓDICAS
 
@@ -43,13 +49,10 @@ void configuracao_Timer0(){
 
 // CONFIGURAÇÃO DAS ROTINAS DE INTERRUPÇÃO
 
-void configura_interrupcao_LDR(){
-  // Configura a ISR para o LDR
-  
-
-}
 void configura_interrupcao_botao_pedestre(){
   // Configura a ISR para o botao_pedestre
+  PCICR |= 0b00000100; // Habilita as insterrupções serem recebidas através dos pinos PCINT16 a PCINT23 (no caso utilizaremos PCINT22).
+  PCMSK2 |= 0b01000000; // Habilita PCINT22 como entrada para receber a interrupção quando nivel ALTO (HIGH) for identificado.
    
 }
 
@@ -58,8 +61,8 @@ void configura_interrupcao_botao_pedestre(){
 void configura_pinos_entrada(){
   // Configura os pinos utilizados como entrada para o LDR e o botão de pedestre.
   
-  pinMode(14, INPUT); // LDR
-  pinMode(6, INPUT); // Botão pedestre
+  pinMode(14, INPUT); // LDR.
+  pinMode(6, INPUT); // Botão pedestre.
   
 }
 
@@ -68,22 +71,22 @@ void configura_pinos_entrada(){
 void configura_pinos_saida(){
   // Configura os pinos utilizados como saida para os LEDs dos semaforos (pedestre e veiculos).
   
-  pinMode(15, OUTPUT); // LED vermelho para o farol de veiculos
-  pinMode(16, OUTPUT); // LED amarelo para o farol de veiculos
-  pinMode(17, OUTPUT); // LED verde para o farol de veiculos
-  pinMode(18, OUTPUT); // LED vermelho para o farol de pedestres
-  pinMode(19, OUTPUT); // LED verde para o farol de pedestres
+  pinMode(15, OUTPUT); // LED vermelho para o farol de veiculos.
+  pinMode(16, OUTPUT); // LED amarelo para o farol de veiculos.
+  pinMode(17, OUTPUT); // LED verde para o farol de veiculos.
+  pinMode(18, OUTPUT); // LED vermelho para o farol de pedestres.
+  pinMode(19, OUTPUT); // LED verde para o farol de pedestres.
 }
 
 // CONFIGURAÇÃO DO DECODIFICADOR DO DISPLAY DE 7 SEGMENTOS
 
 void configura_decodificador(){
-  // Configura os pinos utilizados como saída para decodificador do display de 7 segmentos.
+  // Configura os pinos utilizados como saída para enviar a cadeida binária ao decodificador do display de 7 segmentos.
   
-  pinMode(11, OUTPUT); // Entrada 4
-  pinMode(10, OUTPUT); // Entrada 3
-  pinMode(9, OUTPUT); // Entrada 2
-  pinMode(8, OUTPUT); // Entrada 1
+  pinMode(11, OUTPUT); // Bit 4 
+  pinMode(10, OUTPUT); // Bit 3
+  pinMode(9, OUTPUT); // Bit 2
+  pinMode(8, OUTPUT); // Bit 1 
   
 } 
 
@@ -102,18 +105,144 @@ ISR(TIMER0_COMPA_vect){
   cont++;  
 }
 
-ISR_botao_pedestre(){
+ISR(PCINT2_vect){
   //  Interrupção executada quando o pedestre pressiona o botão solicitando a parada dos carros.
-  botao_pedestre = 1; 
-}
-ISR(){
-  //  Interrupção executada quando houver alteração do periodo do dia.
-  switch (periodo_do_dia){
-    case 0:
-      periodo_do_dia = 1;
-    case 1:
-      periodo_do_dia = 0;
+  
+  if(digitalRead(pino_botao_pedestre) == 1){ // Verifica-se nivel alto no pino da interrupção para indicar que o botão do pedestre foi acionado.
+    botao_pedestre = 1; // Indica-se que o botão do pedestre foi selecionado.
+    count = 0; // Inicia-se a contagem do tempo para  
   }
+  
+}
+
+// TIMERS 
+
+void conta_1_segundo(){
+  // Contador de 1 segundo utilizando as interrupções periodicas geradas pelo timer.
+  cont = 0;
+  while(cont < 500){}
+}
+void conta_meio_segundo(){
+  // Contador de 0.5 segundos utilizando as interrupções periodicas geradas pelo timer.
+  cont = 0;
+  while(cont < 250){}
+
+}
+void conta_3_segundos(){
+  // Contador de 3 segundos utilizando as interrupções periodicas geradas pelo timer.
+  cont = 0;
+  while(cont < 1500){}
+}
+
+// VALORES DO DISPLAY DE 7 SEGMENTOS
+
+void conversor_decimal_binario_para_display_7_segmentos(int valor_decimal){
+  // Converte valores decimais para binário para serem utilizados no decodificador do display de 7 segmentos CD4511.
+
+  switch (valor_decimal) {
+    case 0:
+      digitalWrite(11, LOW);
+      digitalWrite(10, LOW);
+      digitalWrite(9, LOW);
+      digitalWrite(8, LOW);
+      break;
+    
+    case 1:
+      digitalWrite(11, LOW);
+      digitalWrite(10, LOW);
+      digitalWrite(9, LOW);
+      digitalWrite(8, HIGH);
+      break;
+
+    case 2:
+      digitalWrite(11, LOW);
+      digitalWrite(10, LOW);
+      digitalWrite(9, HIGH);
+      digitalWrite(8, LOW);
+      break;
+    
+    case 3:
+      digitalWrite(11, LOW);
+      digitalWrite(10, LOW);
+      digitalWrite(9, HIGH);
+      digitalWrite(8, HIGH);
+      break;
+    
+    case 4:
+      digitalWrite(11, LOW);
+      digitalWrite(10, HIGH);
+      digitalWrite(9, LOW);
+      digitalWrite(8, LOW);
+      break;
+    
+    case 5:
+      digitalWrite(11, LOW);
+      digitalWrite(10, HIGH);
+      digitalWrite(9, LOW);
+      digitalWrite(8, HIGH);
+      break;
+    
+    case 6:
+      digitalWrite(11, LOW);
+      digitalWrite(10, HIGH);
+      digitalWrite(9, HIGH);
+      digitalWrite(8, LOW);
+      break;
+    
+    case 7:
+      digitalWrite(11, LOW);
+      digitalWrite(10, HIGH);
+      digitalWrite(9, HIGH);
+      digitalWrite(8, HIGH);
+      break;
+    
+    case 8:
+      digitalWrite(11, HIGH);
+      digitalWrite(10, LOW);
+      digitalWrite(9, LOW);
+      digitalWrite(8, LOW);
+      break;
+    
+    case 9:
+      digitalWrite(11, HIGH);
+      digitalWrite(10, LOW);
+      digitalWrite(9, LOW);
+      digitalWrite(8, HIGH);
+      break;    
+  }
+}
+
+// FUNÇÃO QUE VERIFICA INSTABILIDADE NA LEITURA DO LDR
+
+void verifica_periodo_do_dia_pelo_LDR(){
+  // Função de varredura para verificar se houve alteração no período do dia e se essa alteração não foi espúria (farol de carro, passaros).
+  
+  valor_LDR = map(analogRead(pino_LDR), 0, 1023, 0, 100); // Le-se o valor do  LDR.
+  conta_3_segundos(); // Espera-se 3 segundos para verificar se a alteração não foi espúria.
+  valor_LDR = map(analogRead(pino_LDR), 0, 1023, 0, 100); // Le-se o valor do  LDR novamente para confirmar o valor medido.
+  if (valor_LDR < 10){ // Se o valor medido for menor que 10 em uma escala de 0 a 100,
+    periodo_do_dia = 1;   // atualiza-se o periodo_do_dia para noite.
+  }
+  else{ // Caso o valor seja maior que 10,
+    periodo_do_dia = 0; // atualiza-se o periodo_do_dia para manhã.
+  }
+}
+
+// MAQUINA DE ESTADOS: DIA E NOITE
+
+void maq_estados_dia_e_noite(){
+  
+  // Define-se a maquina de estados do sistema alternando entre os estados "dia" e "noite" de acordo com o valor lido pelo LDR.
+
+  verifica_periodo_do_dia_pelo_LDR(); // Verifica-se em qual período do dia o sistema está aferindo os dados do LDR. 
+  
+  if (periodo_do_dia == 1){ // Se o período for noturno, chama-se a maquina 
+    maq_estados_noite();
+  } else{
+    maq_estados_dia();
+  }
+
+  
 }
 
 void setup() {
@@ -124,10 +253,15 @@ void setup() {
   configura_pinos_saida(); // CONFIGURA SAIDAS
   configura_decodificador(); // CONFIGURA DECODIFICADOR
   configura_seletor_de_display_de_sete_segmentos(); // CONFIGURA SELETOR DE DISPLAY 7 SEGMENTOS
+  configura_interrupcao_botao_pedestre();
+  digitalWrite(5, HIGH);
+  digitalWrite(4, LOW);
   sei();
 
 }
 
 void loop() {
-  
+
+  maq_estados_dia_e_noite();
+
 }
